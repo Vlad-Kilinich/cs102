823fc5a256712bdaf11c5e48602b400357873661
Vladkilinichh <63118851+Vladkilinichh@users.noreply.github.com>
2020-11-07 19:54:36 +0600

    Update sudoku.py

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -1,5 +1,5 @@
 from typing import Tuple, List, Set, Optional
-
+import random
 
 def read_sudoku(filename: str) -> List[List[str]]:
     """ Прочитать Судоку из указанного файла """
@@ -28,7 +28,15 @@ def group(values: List[str], n: int) -> List[List[str]]:
     >>> group([1,2,3,4,5,6,7,8,9], 3)
     [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     """
-    pass
+    List_new=[]
+    j=0
+    Len=int(len(values)/n)
+    for i in range(n):
+        List_new.append([])
+        for h in range(Len):
+            List_new[i].append(values[j])
+            j+=1
+    return List_new
 
 
 def get_row(grid: List[List[str]], pos: Tuple[int, int]) -> List[str]:
@@ -41,7 +49,8 @@ def get_row(grid: List[List[str]], pos: Tuple[int, int]) -> List[str]:
     >>> get_row([['1', '2', '3'], ['4', '5', '6'], ['.', '8', '9']], (2, 0))
     ['.', '8', '9']
     """
-    pass
+    row, col = pos
+    return grid[row]
 
 
 def get_col(grid: List[List[str]], pos: Tuple[int, int]) -> List[str]:
@@ -54,7 +63,13 @@ def get_col(grid: List[List[str]], pos: Tuple[int, int]) -> List[str]:
     >>> get_col([['1', '2', '3'], ['4', '5', '6'], ['.', '8', '9']], (0, 2))
     ['3', '6', '9']
     """
-    pass
+    row, col = pos
+    A=[]
+    for i in range(len(grid)):
+    	for j in range(len(grid[i])):
+    		if j == col:
+    			A.append(grid[i][j])
+    return A
 
 
 def get_block(grid: List[List[str]], pos: Tuple[int, int]) -> List[str]:
@@ -68,7 +83,18 @@ def get_block(grid: List[List[str]], pos: Tuple[int, int]) -> List[str]:
     >>> get_block(grid, (8, 8))
     ['2', '8', '.', '.', '.', '5', '.', '7', '9']
     """
-    pass
+    row, col = pos
+    string=row//3
+    stlb=col//3
+    A=[]
+    for i in range(stlb*3, stlb*3+3):
+        A.append(grid[string*3][i])
+    for i in range(stlb*3, stlb*3+3):
+        A.append(grid[string*3+1][i])
+    for i in range(stlb*3, stlb*3+3):
+        A.append(grid[string*3+2][i])
+    return A
+    
 
 
 def find_empty_positions(grid: List[List[str]]) -> Optional[Tuple[int, int]]:
@@ -81,7 +107,16 @@ def find_empty_positions(grid: List[List[str]]) -> Optional[Tuple[int, int]]:
     >>> find_empty_positions([['1', '2', '3'], ['4', '5', '6'], ['.', '8', '9']])
     (2, 0)
     """
-    pass
+    pos = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == '.':
+                if pos:
+                    pass
+                else:
+                    pos=(i, j)
+    return(pos)
+    
 
 
 def find_possible_values(grid: List[List[str]], pos: Tuple[int, int]) -> Set[str]:
@@ -95,8 +130,45 @@ def find_possible_values(grid: List[List[str]], pos: Tuple[int, int]) -> Set[str
     >>> values == {'2', '5', '9'}
     True
     """
-    pass
+    main={'1', '2', '3', '4', '5', '6', '7', '8', '9'}
+    A=[]
+    A.extend(get_row(grid, pos))
+    A.extend(get_col(grid, pos))
+    A.extend(get_block(grid, pos))
+    B= set(A)
+    vyvod= main.difference(B)
+    return(vyvod)
 
+
+def check_solution(solution: List[List[str]]) -> bool:
+    """ Если решение solution верно, то вернуть True, в противном случае False """
+    # TODO: Add doctests with bad puzzles
+    main={'1', '2', '3', '4', '5', '6', '7', '8', '9'}
+    l = 0
+    pos =0
+    b=0
+    for i in range(len(solution)):
+        for q in range(len(solution[i])):
+            b+=1
+            pos = (i, q)
+            A=[]
+            AA=[]
+            AAA=[]
+            A.extend(get_row(solution, pos))
+            AA.extend(get_col(solution, pos))
+            AAA.extend(get_block(solution, pos))
+            B=set(A)
+            BB=set(AA)
+            BBB=set(AAA)
+            
+            if (B == main) and (BB==main) and (BBB==main):
+                l+=1
+            else:
+                pass
+    if b == l:
+        return True
+    else:
+        return False
 
 def solve(grid: List[List[str]]) -> Optional[List[List[str]]]:
     """ Решение пазла, заданного в grid """
@@ -111,14 +183,17 @@ def solve(grid: List[List[str]]) -> Optional[List[List[str]]]:
     >>> solve(grid)
     [['5', '3', '4', '6', '7', '8', '9', '1', '2'], ['6', '7', '2', '1', '9', '5', '3', '4', '8'], ['1', '9', '8', '3', '4', '2', '5', '6', '7'], ['8', '5', '9', '7', '6', '1', '4', '2', '3'], ['4', '2', '6', '8', '5', '3', '7', '9', '1'], ['7', '1', '3', '9', '2', '4', '8', '5', '6'], ['9', '6', '1', '5', '3', '7', '2', '8', '4'], ['2', '8', '7', '4', '1', '9', '6', '3', '5'], ['3', '4', '5', '2', '8', '6', '1', '7', '9']]
     """
-    pass
-
-
-def check_solution(solution: List[List[str]]) -> bool:
-    """ Если решение solution верно, то вернуть True, в противном случае False """
-    # TODO: Add doctests with bad puzzles
-    pass
-
+    pos = find_empty_positions(grid)
+    if not pos:
+        return grid
+    possible_values = find_possible_values(grid, pos)
+    for i in possible_values:
+        grid[pos[0]][pos[1]] = i
+        if solve(grid):
+            return(grid)
+        else:
+            grid[pos[0]][pos[1]] = '.'
+    
 
 def generate_sudoku(N: int) -> List[List[str]]:
     """ Генерация судоку заполненного на N элементов
@@ -142,7 +217,27 @@ def generate_sudoku(N: int) -> List[List[str]]:
     >>> check_solution(solution)
     True
     """
-    pass
+    
+    grid=[['.', '.', '.', '.', '.', '.', '.', '.', '.'], ['.', '.', '.', '.', '.', '.', '.', '.', '.'], ['.', '.', '.', '.', '.', '.', '.', '.', '.'], ['.', '.', '.', '.', '.', '.', '.', '.', '.'], ['.', '.', '.', '.', '.', '.', '.', '.', '.'], ['.', '.', '.', '.', '.', '.', '.', '.', '.'], ['.', '.', '.', '.', '.', '.', '.', '.', '.'], ['.', '.', '.', '.', '.', '.', '.', '.', '.'], ['.', '.', '.', '.', '.', '.', '.', '.', '.']]
+    grid= solve(grid)
+    J=81-N
+    while J>0:
+        display(grid)
+        t=random.randint(0, 8)
+        t2=random.randint(0, 8)
+        if grid[t][t2] != '.':
+            grid[t][t2]= '.'
+            J-=1
+
+            
+
+    return grid 
+            
+            
+
+            
+            
+    
 
 
 if __name__ == '__main__':
