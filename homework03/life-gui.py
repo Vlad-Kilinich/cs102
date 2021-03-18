0fd146a1164ddc40b6eab59653f7cd3490dc89c4
Vladkilinichh <63118851+Vladkilinichh@users.noreply.github.com>
2021-03-19 01:23:48 +0600

    Update life-gui.py

--- homework03/life-gui.py
+++ homework03/life-gui.py
@@ -1,88 +1,107 @@
 import pygame
-from pygame.locals import *
-import math
-
 from life import GameOfLife
+from pygame.locals import *
 from ui import UI
 
 
 class GUI(UI):
 
-    def __init__(self, life: GameOfLife, cell_size: int=30, speed: int=1) -> None:
-        super().__init__(GameOfLife)
-
-        self.speed = speed
+    def __init__(self, life: GameOfLife, cell_size: int=30, speed: int=10) -> None:
+        super().__init__(life)
         self.cell_size = cell_size
+        self.width = self.life.rows * self.cell_size
+        self.height = self.life.cols * self.cell_size
 
-        self.life = life
-
-        self.screen_size =  self.life.rows * cell_size, self.life.cols * cell_size
+        # Устанавливаем размер окна
+        self.screen_size = self.width, self.height
+        # Создание нового окна
         self.screen = pygame.display.set_mode(self.screen_size)
 
+        # Вычисляем количество ячеек по вертикали и горизонтали
+        self.cell_width = self.width // self.cell_size
+        self.cell_height = self.height // self.cell_size
+
+        # Скорость протекания игры
+        self.speed = speed
 
     def draw_lines(self) -> None:
-        """ Отрисовать сетку """
-        for x in range(0, self.cell_size, self.cell_size):
+        for x in range(0, self.width, self.cell_size):
             pygame.draw.line(self.screen, pygame.Color('black'),
-                    (x, 0), (x, self.cell_size))
-        for y in range(0, self.cell_size, self.cell_size):
+                    (x, 0), (x, self.height))
+        for y in range(0, self.height, self.cell_size):
             pygame.draw.line(self.screen, pygame.Color('black'),
-                    (0, y), (self.cell_size, y))
-
+                    (0, y), (self.width, y))
 
     def draw_grid(self) -> None:
-        """
-        Отрисовка списка клеток с закрашиванием их в соответствующе цвета.
-        """
-        y = 0
-        grid = self.life.curr_generation
-
-        for i in grid:
-            x = 0
-            for v in i:
-                color = 'white' if v == 0 else 'green'
-                pygame.draw.rect(self.screen, pygame.Color(color),
-                                    pygame.Rect(x, y, self.cell_size, self.cell_size))
-                x += self.cell_size
-            y += self.cell_size
-
+        # Copy from previous assignment
+        for height in range(self.life.rows):
+            for width in range(self.life.cols):
+                x=width*self.cell_size
+                y=height*self.cell_size
+                a = self.cell_size
+                if self.life.curr_generation[height][width] ==0:
+                    pygame.draw.rect(self.screen, pygame.Color('white'), (x,y,a,a))
+                else:
+                    pygame.draw.rect(self.screen, pygame.Color('green'), (x,y,a,a))
+                    
 
     def run(self) -> None:
-        """ Запустить игру """
+        # Copy from previous assignment
         pygame.init()
         clock = pygame.time.Clock()
         pygame.display.set_caption('Game of Life')
         self.screen.fill(pygame.Color('white'))
 
-        running = True
-        pause = False
+        # Создание списка клеток
+        self.life.create_grid(randomize=True)
 
-        while running:
+        pause = False
+        running = True
+        while running and (self.life.is_max_generations_exceeded is False) and (self.life.is_changing is True):
             for event in pygame.event.get():
                 if event.type == QUIT:
                     running = False
-                elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_SPACE:
-                        pause = True if pause == False else False
-                elif event.type == pygame.MOUSEBUTTONDOWN:
-                    if event.button == 1:
-                        x, y = event.pos
-                        row = math.floor(x/self.cell_size)
-                        col = math.floor(y/self.cell_size)
-                        self.life.curr_generation[col][row] = 1 if self.life.curr_generation[col][row] == 0 else 0
-                        self.draw_grid()
-
-            if not self.life.is_changing and self.life.randomize == True:
-                running = False
-
-            if not pause:
-                self.draw_lines()
-
-                self.life.step()
-                if self.life.generations > self.life.max_generations:
-                    break
+                elif event.type == KEYDOWN:
+                    pause = not pause
+                elif event.type == MOUSEBUTTONUP:
+                    i, j = event.pos
+                    i = i // self.cell_size
+                    j = j // self.cell_size
+                    if self.life.curr_generation[j][i] == 0:
+                        self.life.curr_generation[j][i] = 1
+                    else:
+                        self.life.curr_generation[j][i] = 0
+                    self.draw_grid()
+                    pygame.display.flip()
+            if pause:
                 self.draw_grid()
+                self.draw_lines()
+                pygame.display.flip()
+                continue
+
+            # Отрисовка списка клеток
+            # Выполнение одного шага игры (обновление состояния ячеек)
+            self.draw_grid()
+            self.draw_lines()
+            self.life.step()
 
             pygame.display.flip()
             clock.tick(self.speed)
         pygame.quit()
+
+import sys
+import argparse
+def createParser ():
+    parser = argparse.ArgumentParser()
+    parser.add_argument ('-w','--width', type=int, default='600')
+    parser.add_argument ('-he','--height', type=int, default='600')
+    parser.add_argument ('-c','--cell_size', type=int, default='30')
+    return parser
+
+
+if __name__ == '__main__':
+    parser = createParser()
+    namespace = parser.parse_args(sys.argv[1:])
+    
+    ui = GUI(GameOfLife((namespace.width // namespace.cell_size, namespace.height// namespace.cell_size), True, 40))
+    ui.run()
